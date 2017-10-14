1d0f96398d638092b10d27322b073df6702e6972
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-10-14 13:47:17 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/servidor/EscuchaCliente.java
+++ src/servidor/EscuchaCliente.java
@@ -2,7 +2,8 @@ package servidor;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.net.ServerSocket;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class EscuchaCliente extends Thread{
@@ -10,11 +11,23 @@ public class EscuchaCliente extends Thread{
 	BufferedReader entrada;
     DataOutputStream salida;
     Socket clientesocket;
+    Sala sala;
     
-    public EscuchaCliente(Socket cs) {
+    public EscuchaCliente(Socket cs,Sala sala) throws IOException {
     	this.clientesocket=cs;
+    	this.entrada=new BufferedReader(new InputStreamReader(clientesocket.getInputStream()));
+    	this.salida=new DataOutputStream(clientesocket.getOutputStream());
+    	this.sala=sala;
     }
     
-    
+    public void run() {
+		try {
+			String mensaje=entrada.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+    }
 	
 }
